19eb283f34184be06b8e6ac04360c53781cee567
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2023-03-17 07:24:42 -0600

    Fusión del menú en mobile

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
-navEmail.addEventListener('click', toggleEmail);
+navEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
-function toggleEmail() {
+function toggleDesktopMenu() {
 	desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+	mobileMenu.classList.toggle('inactive');
 }
